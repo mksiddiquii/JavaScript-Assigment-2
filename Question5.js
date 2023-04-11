431e14c4d7638d21339a5edd595b8834069ce22d
mksiddiquii <119476589+mksiddiquii@users.noreply.github.com>
2023-04-11 16:02:22 +0500

    Add files via upload

---
+++ Question5.js
@@ -0,0 +1,6 @@
+function newParagrph( color){
+ myPara=document.getElementById("my-para")
+myPara.innerText=(prompt("enter your paragraph"));
+myPara.style.color=(prompt("enter your color"));
+myPara.style.backgroundColor="#000"
+}
